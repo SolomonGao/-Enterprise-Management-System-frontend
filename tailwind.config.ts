58c618af3c8822053c326640aebedb92f2b6f8b3
Xing gao <738234123@qq.com>
2024-12-13 12:15:48 +0800

    push

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,29 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
+      fontFamily:{
+        Poppins: ["var(--font-Poppins)"],
+        Josefin: ["var(--font-Josefin)"],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      screens: {
+        "1000px": "1000px",
+        "1100px": "1100px",
+        "1200px": "1200px",
+        "1300px": "1300px",
+        "1500px": "1500px",
+        "800px": "800px",
+        "400px": "400px",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        customHover: '#111827',
       },
     },
   },
