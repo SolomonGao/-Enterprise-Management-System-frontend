471c1bb72e3a4d59e9000a2e894bce3344ff05b3
Xing gao <738234123@qq.com>
2025-01-04 14:53:36 +0800

    123

---
+++ app/profile/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React, { FC, useState } from 'react'
+import Protected from '../hooks/useProtected';
+import Header from '../components/Header';
+import Heading from '../utils/Heading';
+import Profile from '../components/Profile/Profile'
+import { useSelector } from 'react-redux';
+
+
+interface Props {}
+
+const ProfilePage: FC<Props> = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(6);
+  const [route, setRoute] = useState("Login");
+  const {user} = useSelector((state: any) => state.auth);
+
+  return (
+    <div className=''>
+      <Protected>
+        <Heading
+          title={`${user.name} 用户中心-`}
+          description="test test"
+          keywords="Programming, Mern, Redux"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+
+        <Profile 
+        user={user}
+        >
+
+        </Profile>
+      </Protected>
+    </div>
+  )
+}
+
+export default ProfilePage;
--- app/reset_password/[token]/page.tsx
+++ app/reset_password/[token]/page.tsx
@@ -1,17 +1,26 @@
 import ResetPassword from "@/app/components/Auth/PostResetPassword";
 import { Box } from "@mui/material";
 
-export default function ResetPasswordPage({ params }: { params: { token: string } }) {
-    const { token } = params;
-    console.log(token)
+// Fetch the token from the context in getServerSideProps
+export async function getServerSideProps({ params }: { params: { token: string } }) {
+  const { token } = params; // Extract the token from URL params
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800">
-            <Box className="flex items-center justify-center w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
-                <ResetPassword token={token} />
-            </Box>
-        </div>
+  // Return the token as a prop
+  return {
+    props: {
+      token,
+    },
+  };
+}
 
+export default function ResetPasswordPage({ token }: { token: string }) {
+  console.log(token); // Token will now be correctly passed as a prop
 
-    );
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800">
+      <Box className="flex items-center justify-center w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+        <ResetPassword token={token} />
+      </Box>
+    </div>
+  );
 }
