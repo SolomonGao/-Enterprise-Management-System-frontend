56fa07cb7a713794dd455c69fc4490426b3b74ae
Xing gao <738234123@qq.com>
2025-02-03 19:33:28 -0500

    fix profile bug

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,42 +1,44 @@
 'use client'
 import React, { FC, useState } from 'react'
-import Protected from '../hooks/useProtected';
 import Header from '../components/Header';
 import Heading from '../utils/Heading';
 import Profile from '../components/Profile/Profile'
 import { useSelector } from 'react-redux';
+import { redirect } from 'next/navigation';
 
 
-interface Props {}
+
+interface Props { }
 
 const Page: FC<Props> = (props: Props) => {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(6);
   const [route, setRoute] = useState("Login");
-  const {user} = useSelector((state: any) => state.auth);
+  const { user } = useSelector((state: any) => state.auth);
 
+  if (!user) {
+    redirect("/");
+  }
   return (
     <div className=''>
-      <Protected>
-        <Heading
-          title={`${user.name} 用户中心-`}
-          description="test test"
-          keywords="Programming, Mern, Redux"
-        />
-        <Header
-          open={open}
-          setOpen={setOpen}
-          activeItem={activeItem}
-          setRoute={setRoute}
-          route={route}
-        />
+      <Heading
+        title={`${user.name} 用户中心-`}
+        description="test test"
+        keywords="Programming, Mern, Redux"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute={setRoute}
+        route={route}
+      />
 
-        <Profile 
+      <Profile
         user={user}
-        >
+      >
 
-        </Profile>
-      </Protected>
+      </Profile>
     </div>
   )
 }
